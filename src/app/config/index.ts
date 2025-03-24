9c0ce4b083904666fdf303933134a089e53951f1
arafat717 <arafatjibon33@gmail.com>
2025-03-24 11:35:18 +0600

    added solved server

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.join((process.cwd(), ".env")) });
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-  port: process.env.PORT,
+  port: process.env.PORT || 5002,
   database_url: process.env.MONGODB_URL,
 };
