f24e4828069e6793039358db789937702b9ac0de
arafat717 <arafatjibon33@gmail.com>
2025-05-15 15:29:58 +0600

    added udate the course

--- src/app/modules/OfferedCourse/OfferedCourse.validation.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.validation.ts
@@ -1,6 +1,16 @@
 // validations/offeredCourse.validation.ts
 import { z } from "zod";
 
+const tiemStringSchema = z.string().refine(
+  (time) => {
+    const regex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid time format, expected "HH:MM" in 24 hours format',
+  }
+);
+
 const createOfferedCourseZodSchema = z.object({
   body: z
     .object({
@@ -12,24 +22,8 @@ const createOfferedCourseZodSchema = z.object({
       maxCapacity: z.number(),
       section: z.number(),
       days: z.array(z.enum(["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"])),
-      startTime: z.string().refine(
-        (time) => {
-          const regex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid time format, expected "HH:MM" in 24 hours format',
-        }
-      ),
-      endTime: z.string().refine(
-        (time) => {
-          const regex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid time format, expected "HH:MM" in 24 hours format',
-        }
-      ),
+      startTime: tiemStringSchema,
+      endTime: tiemStringSchema,
     })
     .refine(
       (body) => {
@@ -45,13 +39,25 @@ const createOfferedCourseZodSchema = z.object({
 });
 
 const updateOfferedCourseZodSchema = z.object({
-  body: z.object({
-    faculty: z.string().optional(),
-    maxCapacity: z.number().optional(),
-    days: z.enum(["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]).optional(),
-    startTime: z.string().optional(),
-    endTime: z.string().optional(),
-  }),
+  body: z
+    .object({
+      faculty: z.string(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum(["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"])),
+      startTime: tiemStringSchema,
+      endTime: tiemStringSchema,
+    })
+    .refine(
+      (body) => {
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+        return end > start;
+      },
+      {
+        message: "End time must be later than start time",
+        path: ["endTime"],
+      }
+    ),
 });
 
 export const offeredCourseValidation = {
